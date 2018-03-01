614349fd4b0adc68ecf5848a394174c0197e87ae
Matyáš Skalický <matyaskalicky@gmail.com>
2018-03-01 13:54:24 +0100

    basic sorting now works

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 from flask import Flask, render_template, request
 from parse_cars import parse_csv
+from pprint import pprint
+import operator
 app = Flask(__name__)
+
+# The database containing the info about the cars
+database = []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    content = ""
+    sort_parameter = ""
     if request.method == "GET":
-        content = request.args.get('search')
+        sort_parameter = request.args.get('sortby')
 
-    if content is None:
-        content = ""
+    database = parse_csv()
 
-    cars_dict = parse_csv()
-    print(cars_dict)
+    if sort_parameter == "name":
+        database.sort(key=operator.itemgetter('name'), reverse=False)
+    elif sort_parameter == "acceleration":
+        database.sort(key=operator.itemgetter('acceleration'), reverse=True)
+    elif sort_parameter == "speed":
+        database.sort(key=operator.itemgetter('speed'), reverse=True)
+    elif sort_parameter == "handling":
+        database.sort(key=operator.itemgetter('handling'), reverse=True)
 
-    return render_template("index.html", title='Home', content=content, dictionary=cars_dict, count=len(cars_dict['name']))
+    return render_template("index.html", title='Index', database=database)
+
+@app.route('/about')
+def about():
+    return render_template("about.html", title='About')
 
 
 if __name__ == '__main__':
+    database = parse_csv()
     app.run(debug=True)
--- parse_cars.py
+++ parse_cars.py
@@ -3,21 +3,28 @@ import sys
 
 # http://nfs.wikia.com/wiki/Need_for_Speed:_Underground_2/Cars
 csv_path = "data/nsfmw2/cars_nsfmw2.csv"
+keys = ["image","name","manufacturer","model","unlock","acceleration",
+        "speed","handling","hundred","topspeed","torque","power"]
+
+def index_from_key(key):
+    for k in range(0, len(keys)):
+        if key == keys[k]:
+            return k
+    return None
 
 def parse_csv():
+    # Array of dictionaries
     all_cars = [ ]
     with open(csv_path) as csvfile:
         readCSV = csv.reader(csvfile, delimiter='|')
         for row in readCSV:
+            r = dict(zip(keys, row))
             try:
-                all_cars.append(row)
+                all_cars.append(r)
             except csv.Error as e:
                 print(e)
-
-    # Transform into a dictionary
-    keys = ["image","name","manufacturer","model","unlock","acceleration","speed","handling","hundred","topspeed","torque","power"]
-    return dict(zip(keys, zip(*all_cars)))
+    return all_cars
 
 if __name__ == "__main__":
-    print(parse_csv())
-
+    parsed = parse_csv()
+    parsed[ 0 ][ "name" ]
